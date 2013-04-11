ebec27057bf025fc26bed66300c10a6c8c4e16d7
matts134 <mshopsin@gmail.com>
2013-04-11 13:44:40 -0700

    Can Add users

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,11 +1,20 @@
 class EmployeesController < ApplicationController
   def index
+    @employees = Employee.all
   end
 
   def new
+    @employee = Employee.new
+    @employee.build_employee_profile
   end
 
   def create
+    @employee = Employee.new(params[:employee])
+    if @employee.save
+      redirect_to employees_path
+    else
+      render 'new'
+    end
   end
 
   def show
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,14 @@
 class Employee < ActiveRecord::Base
-  attr_accessible :employee_profile_id, :supervisor_id
+  attr_accessible :employee_profile_id, :supervisor_id, :employee_profile_attributes
   
   has_many :team_memberships
-  belongs_to :supervisor
-   accepts_nested_attributes_for :employee_profile, :
+  has_one :employee_profile
+  belongs_to :supervisor, :class_name => "Employee"
+   accepts_nested_attributes_for :employee_profile
+   
+   
+   def name
+     employee_profile.name
+   end
+   
 end
