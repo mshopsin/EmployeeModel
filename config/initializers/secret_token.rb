0028257269034800e4451a7f6d79b512613ce015
matts134 <mshopsin@gmail.com>
2013-04-11 09:20:56 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmployeeDB::Application.config.secret_token = '1166c858c3ac52bd2d938cbb7a61579fd1f39676f084b40e4a8e96ce81ac84ffba5539a24e2e4ac2725a5a9262fbe5b890e2822e76be1580cbd08e008bc50a5d'
